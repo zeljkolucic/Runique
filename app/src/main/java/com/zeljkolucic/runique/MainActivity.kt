11b5d27ab4fb443956f3fdcf61453e46d3292be7
Zeljko Lucic <zeljko.lucic99@gmail.com>
2025-10-30 23:33:42 +0100

    Implement Material Theme

--- app/src/main/java/com/zeljkolucic/runique/MainActivity.kt
+++ app/src/main/java/com/zeljkolucic/runique/MainActivity.kt
@@ -3,45 +3,26 @@ package com.zeljkolucic.runique
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.zeljkolucic.runique.ui.theme.RuniqueTheme
+import com.zeljkolucic.core.presentation.designsystem.AnalyticsIcon
+import com.zeljkolucic.core.presentation.designsystem.RuniqueTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             RuniqueTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Icon(imageVector = AnalyticsIcon, contentDescription = null)
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    RuniqueTheme {
-        Greeting("Android")
     }
 }
--- core/presentation/designsystem/build.gradle.kts
+++ core/presentation/designsystem/build.gradle.kts
@@ -9,9 +9,9 @@ android {
 dependencies {
 
     implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
+    implementation(libs.androidx.compose.ui)
+    implementation(libs.androidx.compose.ui.graphics)
+    implementation(libs.androidx.compose.ui.tooling.preview)
+    debugImplementation(libs.androidx.ui.tooling)
+    api(libs.androidx.compose.material3)
 }
